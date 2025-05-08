9cf4719d30f99bbef1880adbf1943603cd250ae6
root <root@vultr.guest>
2025-05-08 11:50:17 +0000

    movie结果

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup python run_llm_compiler.py --benchmark movie --store ./log --logging --stream
+nohup python run_llm_compiler.py --benchmark movie --store ./log --logging --stream > output.log 2>&1 &
