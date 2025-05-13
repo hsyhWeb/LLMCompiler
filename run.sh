586b26f061eed6bbc257198c4c5a1351520b9076
root <root@vultr.guest>
2025-05-13 09:39:40 +0800

    hotpotqa

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup python run_llm_compiler.py --benchmark movie --store ./log --logging --stream > output.log 2>&1 &
+nohup python run_llm_compiler.py --benchmark hotpotqa --store ./log_hotpotqa --logging --stream > output_hotpotqa.log 2>&1 &
