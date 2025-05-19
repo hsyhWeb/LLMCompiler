84731545447b86ee37369a616f2cf122d2723998
root <root@vultr.guest>
2025-05-19 14:34:58 +0800

    parallelqa

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup python run_llm_compiler.py --benchmark parallelqa --store ./log_parallelqa --logging --stream > output_parallelqa.log 2>&1 &
+python run_llm_compiler.py --benchmark parallelqa --store ./log_parallelqa --logging --stream 
